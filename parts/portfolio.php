e0d8612a4368659d97f1c498cfd031e9e65ed043
sshamseddin <Shayan.Shamseddin@rexx-systems.com>
2020-08-14 15:22:17 +0200

    add parts shablog

---
+++ parts/portfolio.php
@@ -0,0 +1,67 @@
+<section class="portfolio" id="portfolio">
+  <div class="container">
+    <h3 class="text-center">Unsere Referenzen</h3>
+    <p class="text-center text-muted font-italic">Diese Referenzen sprechen für uns.</p>
+  
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/adventure-daylight-girl-1462014.jpg" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Webseiten-Projekt</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">Bei einem Großkunden</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-6 col-md-4">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/aerial-view-color-daylight-1125278.jpg" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Webseiten-Projekt</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">Bei einem Großkunden</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-6 col-md-4">
+          <a href="../referenzen/kreationen/index.html">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/architecture-basilica-buildings-326709.jpg" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Kreativ-Projekt</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">Junky Monkey</h6>
+          </div>
+        </div></a>
+      </div>
+      <div class="col-12 col-sm-6 col-md-4">
+          <a href="../referenzen/spiele/index.html">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/architecture-building-buildings-264706.jpg" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Spiele</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">JavaScript Spiele</h6>
+          </div>
+        </div></a>
+      </div>
+      <div class="col-12 col-sm-6 col-md-4">
+          <a href="../referenzen/website-muster/Resp-Webseite.html">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/ballpen-blur-bokeh-834897.jpg" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Webseiten-Projekt</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">Weisite-Muster</h6>
+          </div>
+        </div></a>
+      </div>
+      <div class="col-12 col-sm-6 col-md-4">
+          <a href="../referenzen/kryptlog">
+        <div class="card mb-4">
+          <img class="card-img-top" src="images/placeholders/portfolio/kryptlog.png" alt="Platzhalter-Bild">
+          <div class="card-body">
+            <h5 class="card-title text-center">Kryptlog</h5>
+            <h6 class="card-subtitle mb-2 text-muted text-center">Ver-Entschlüsseln(Microservices)</h6>
+          </div>
+        </div></a>
+      </div>
+    </div>
+  </div>
+</section>
