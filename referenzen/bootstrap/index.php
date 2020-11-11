0e31735f72c56c5c375f1b656b356b40f47c3789
sshamseddin <Shayan.Shamseddin@rexx-systems.com>
2020-11-11 14:36:08 +0100

    create class secretDairy

--- referenzen/bootstrap/index.php
+++ referenzen/bootstrap/index.php
@@ -188,10 +188,10 @@ if($_POST){
 <div class="row">
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-            <a href="secretDairy/index.php"><img class="card-img-top" src="krypt.jpg" alt="..."></a>
+            <a href="secretDairy/index.php"><img class="card-img-top" src="carnival.jpg" alt="..."></a>
             <div class="caption">
-                <h3>Kryptlog</h3>
-                <p>Eine Seite zum Ver-Entschl&uuml;sseln der Nachrichten mit historischen Verfahren.</p>
+                <h3>Secret Dairy</h3>
+                <p>Ein geheimes Tagebuch um schöne Erinnerungen einzuspeichern.</p>
 
 
             </div>
