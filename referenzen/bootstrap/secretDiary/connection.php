515e1782b83c367de2e658a20e6f42a2f625454c
sshamseddin <Shayan.Shamseddin@rexx-systems.com>
2020-11-12 18:18:58 +0100

    create connection to db fpr secretdairy

---
+++ referenzen/bootstrap/secretDiary/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect("localhost", "3airjxylbw", "Isahamburg99##", "mhbjvvqf0");
+        
+        if(mysqli_connect_error()){
+            
+            die("Verbindung zur Datenbank konnte nicht aufgebaut werden.");
+            
+        }
+
+?>
