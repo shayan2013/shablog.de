8feeeb68871acacaeceda997362161f7550f00a3
sshamseddin <Shayan.Shamseddin@rexx-systems.com>
2020-11-13 12:04:28 +0100

    create header for secretDairy

---
+++ referenzen/bootstrap/secretDiary/header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Die 3 Meta-Tags oben *müssen* zuerst im head stehen; jeglicher sonstiger head-Inhalt muss *nach* diesen Tags kommen -->
+    <title>Geheimes Tagebuch</title>
+
+          <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      
+    <!-- Das neueste kompilierte und minimierte CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optionales Theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Das neueste kompilierte und minimierte JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+      
+      <style type="text/css">
+      
+        
+          .container{
+              
+              text-align: center;
+              width: 400px;
+              
+              
+          }
+          
+          #homepageContainer{
+              
+              margin-top:150px;
+              
+          }
+          
+          
+          html { 
+              background: url(background.jpg) no-repeat center center fixed; 
+              -webkit-background-size: cover;
+              -moz-background-size: cover;
+              -o-background-size: cover;
+              background-size: cover;
+            }
+      
+          
+          body{
+              
+              background: none;
+              color: white;
+              
+          }
+          
+          #loginForm{
+              
+              display:none;
+              
+          }
+          
+          #diary{
+              
+              width: 100%;
+              height: 500px;
+              
+              
+          }
+      
+      
+      </style>
+      
+      
+      
+      
+  </head>
+    
+<body>
