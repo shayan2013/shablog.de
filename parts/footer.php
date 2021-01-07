a505e8b23e5f2bce3a418d6205a957139bd94842
sshamseddin <Shayan.Shamseddin@rexx-systems.com>
2021-01-07 18:05:04 +0100

    add privacy impressum to footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,9 +1,11 @@
 <footer class="footer pt-md-3 pb-md-2">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12 col-md-4">
+
+        <div class="col-12 col-md-4">
         <p class="mt-3 text-center">© ShaBlog-Agentur <?php echo date("Y") ?></p>
       </div>
+
       <div class="col-12 col-md-4">
         <div class="d-flex justify-content-center mb-3 mt-md-2">
           <a class="footer-icon d-flex flex-column justify-content-center" href="#">
@@ -20,6 +22,15 @@
           </a>
         </div>
       </div>
+
+        <div id="footer_links" class="col-12 col-md-4">
+            <div class="d-flex justify-content-center mb-3 mt-md-2">
+                <a style="color:#000;margin:5px" href="http://shablog.de/referenzen/impressum/index.html" target="_blank">Impressum</a>
+                <a style="color:#000;margin:5px" href="http://shablog.de/referenzen/datenschutz/index.html" target="_blank">Datenschutz</a>
+
+            </div>
+        </div>
+
     </div>
   </div>
 </footer>
